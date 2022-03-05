904664e52e7868c6335610d8282844d60c3a0a0f
tommy89231671 <tommy89231671@gmail.com>
2022-03-04 20:24:18 -0800

    flix2 update

---
+++ flix/flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Tommyyu on 3/4/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
