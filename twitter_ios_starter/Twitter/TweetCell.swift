aa2e3dc4f6349e856023697e1d2b1deac083081a
tommy89231671 <tommy89231671@gmail.com>
2022-03-10 02:13:42 -0800

    update twitter_ios

---
+++ twitter_ios_starter/Twitter/TweetCell.swift
@@ -0,0 +1,30 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Tommyyu on 3/9/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
