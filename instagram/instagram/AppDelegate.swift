8ddaddb28a8bc41b8273284b6c047ef311f3dcba
tommy89231671 <tommy89231671@gmail.com>
2022-04-01 01:38:23 -0700

    update part2

--- instagram/instagram/AppDelegate.swift
+++ instagram/instagram/AppDelegate.swift
@@ -20,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                     $0.server = "https://parseapi.back4app.com"
             }
         Parse.initialize(with: parseConfig)
+        
+        
         return true
     }
 
